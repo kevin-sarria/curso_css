d42df8d33ca930763e21d76591307ad018789f48
kevin-sarria <81386916+kevin-sarria@users.noreply.github.com>
2023-01-01 20:24:35 -0500

    avances

---
+++ 11-cafeteria/gulpfile.js
@@ -0,0 +1,28 @@
+const { src, dest, watch }  = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+
+function css( done ) {
+
+
+    src('src/scss/app.scss')
+        .pipe( sass({ outputStyle: 'compressed' }) )
+        .pipe( postcss( [ autoprefixer() ] ) ) 
+        .pipe( dest('build/css') )
+
+    done();
+
+}
+
+function dev() {
+
+    watch( 'src/scss/app.scss', css );
+
+}
+
+
+
+exports.css = css;
+exports.dev = dev;
