524fec6abfb8314c068012365214cf0cffba599e
kevin-sarria <81386916+kevin-sarria@users.noreply.github.com>
2023-01-03 01:35:44 -0500

    avances

--- 11-cafeteria/gulpfile.js
+++ 11-cafeteria/gulpfile.js
@@ -1,24 +1,64 @@
-const { src, dest, watch }  = require('gulp');
+// CSS y SASS
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
+const autoPrefixer = require('autoprefixer');
 
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
-function css( done ) {
+function css() {
 
+    return src('src/scss/app.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoPrefixer(),cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'));
 
-    src('src/scss/app.scss')
-        .pipe( sass({ outputStyle: 'compressed' }) )
-        .pipe( postcss( [ autoprefixer() ] ) ) 
-        .pipe( dest('build/css') )
+}
 
-    done();
+function imagenes() {
+
+    return src('src/img/**/*')
+        .pipe( imagemin({ optimizationLevel: 3 }) )
+        .pipe(dest('build/img'))
+
+}
+
+function versionWebp() {
+
+    const opciones = {
+        quality: 50
+    }
+
+    return src('src/img/**/*.{png,jpg}')
+            .pipe( webp(opciones) )
+            .pipe( dest('build/img') )
+
+}
+
+function versionAvif() {
+
+    const opciones = {
+        quality: 50
+    }
+
+    return src('src/img/**/*.{png,jpg}')
+            .pipe( avif(opciones) )
+            .pipe( dest('build/img') )
 
 }
 
 function dev() {
 
-    watch( 'src/scss/app.scss', css );
+    watch('src/scss/**/*.scss', css);
+    watch( 'src/img/**/*', imagenes );
 
 }
 
@@ -26,3 +66,10 @@ function dev() {
 
 exports.css = css;
 exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = parallel( imagenes, versionWebp, versionAvif, css, dev );
+
+// Series - Se inicia una tarea, y hasta que finaliza inicia la siguiente
+// Parallel - Todas se inician al mismo tiempo
