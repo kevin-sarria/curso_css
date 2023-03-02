5832e0f4ddd7a365d73acc87cffa7da8f67e8f65
Kevin Sarria <reysabueso256@gmail.com>
2023-03-02 01:09:32 -0500

    Proyecto Terminado: Proyecto Real State con HTML, SASS, Gulp y un poco de JavaScript, Terminado

---
+++ 15-realState/js/app.js
@@ -0,0 +1,12 @@
+const imagenes = document.querySelectorAll('.propiedad__imagen');
+
+window.addEventListener( 'scroll', () => {
+
+    const scroll = this.scrollY / -40;
+
+    imagenes.forEach( imagen => {
+        imagen.style.backgroundPositionY = `${scroll}px`;
+    } );
+
+});
+
